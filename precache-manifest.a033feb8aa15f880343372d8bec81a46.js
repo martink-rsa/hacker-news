8dbb60f79d2e719023ac4464d1894b110ebcab20
Martin Kruger <martin@martinkruger.com>
2020-02-24 19:17:27 +0200

    Updates

---
+++ precache-manifest.a033feb8aa15f880343372d8bec81a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0341abe8342ff7d3adda42cb5e9bda64",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "9a46e73c0113cb46644b",
+    "url": "/hacker-news/static/css/main.e6dce7c2.chunk.css"
+  },
+  {
+    "revision": "8b7167667c773d9e90e6",
+    "url": "/hacker-news/static/js/2.b32dff4b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/hacker-news/static/js/2.b32dff4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a46e73c0113cb46644b",
+    "url": "/hacker-news/static/js/main.7439aa6a.chunk.js"
+  },
+  {
+    "revision": "84dd1e59cbdf94cf1764",
+    "url": "/hacker-news/static/js/runtime-main.e10be352.js"
+  }
+]);
